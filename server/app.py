593816dfa7184c4b09b1b466e31bbaa7ff40c1cf
Ajay-Ganapathy <koodaloorajayvijay@gmail.com>
2026-04-06 16:57:30 +0530

    Added host argument in args parser

--- server/app.py
+++ server/app.py
@@ -74,11 +74,11 @@ def main():
     import argparse
 
     parser = argparse.ArgumentParser()
-    host = "0.0.0.0"
+    parser.add_argument("--host" , type=str , default="0.0.0.0")
     parser.add_argument("--port", type=int, default=8000)
     args = parser.parse_args()
 
-    uvicorn.run(app, host=host, port=args.port)
+    uvicorn.run(app, host=args.host, port=args.port)
 
 
 if __name__ == "__main__":
